e866b2068099c888000ab919c8008b0ab10005d9
danymorar20 <danymorar20@gmail.com>
2025-07-19 20:27:47 -0600

    fixing duplicated hashing proccesss

--- src/features/auth/application/use-cases/signup.use-case.ts
+++ src/features/auth/application/use-cases/signup.use-case.ts
@@ -1,5 +1,4 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { PasswordHasherService } from '@hasher-service/password-hasher.service';
 import { JwtService } from '@nestjs/jwt';
 import { SignupDto } from '@auth/application/dtos/signup.dto';
 import { AuthResponseDto } from '@auth/application/dtos/auth-response.dto';
@@ -9,24 +8,18 @@ import { CreateUserUseCase } from '@users/application/use-cases/create-user.use-
 @Injectable()
 export class SignupUseCase {
   constructor(
-    private readonly passwordHasher: PasswordHasherService,
     private readonly jwtService: JwtService,
-    private readonly findUserByEmailUseCase: FindUserByEmailUseCase,
     private readonly createUserUseCase: CreateUserUseCase,
+    private readonly findUserByEmailUseCase: FindUserByEmailUseCase,
   ) {}
 
-  async execute(dto: SignupDto): Promise<AuthResponseDto> {
-    const exists = await this.findUserByEmailUseCase.execute(dto.email);
+  async execute(newUserDto: SignupDto): Promise<AuthResponseDto> {
+    const exists = await this.findUserByEmailUseCase.execute(newUserDto.email);
     if (exists) throw new BadRequestException('Email already registered');
 
-    const hash = await this.passwordHasher.hash(dto.password);
     const user = await this.createUserUseCase.execute({
-      email: dto.email,
-      password: hash,
+      ...newUserDto,
       status: true,
-      name: dto.name,
-      lastName: dto.lastName,
-      cellphone: dto.cellphone,
     });
 
     const payload = { sub: user.id, email: user.email };
